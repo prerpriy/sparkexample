d25637464a0b1bb46bde821c02b62673e9ed1c94
prerna <prerna.aug@gmail.com>
2017-01-05 15:03:00 -0600

    new

--- src/main/java/sparkexample/Hello.java
+++ src/main/java/sparkexample/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 
     public static void main(String[] args) {
         get("/", (req, res) -> {
-            return "hello from sparkjava.com";
+            return "happy new year from sparkjava.com";
         });
     }
 }
